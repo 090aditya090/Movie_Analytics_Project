dea21e3f42f645690fce157083a131ea5d65bc09
090aditya090 <asralfa9@gmail.com>
2023-03-09 12:02:36 +0530

    First Commit of Spark_DataFrames.py

---
+++ Spark_DataFrames.py
@@ -0,0 +1,12 @@
+#                                           Spark Data Frames
+#==============================================================================================================================
+# 1. Prepare Movies data: Extracting the Year and Genre from the Text
+# 2. Prepare Users data: Loading a double delimited csv file
+# 3. Prepare Ratings data: Programmatically specifying a schema for the data frame
+# 4. Import Data from URL: Scala
+# 5. Save table without defining DDL in Hive
+# 6. Broadcast Variable example
+# 7. Accumulator example
+
+#=============================================================================================================================
+#=============================================================================================================================
